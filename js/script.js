d603a11c306ede80ba42b657cfd3d05bca65a455
Thunderbird93 <antircristian@gmail.com>
2022-10-12 16:55:58 +0200

    add js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const agePassenger = prompt ('Quanti anni hai?');
+const distanceKm = prompt ('Quanti km vuole percorrere?');
+
+let scontoGiovani = 0.2; // 17 anni o meno
+let scontoAnziani = 0.4; // 66 anni o più
+let kmPrice = 0.21;
+let scontoTotaleA = kmPrice * distanceKm * scontoAnziani;
+let scontoTotaleS = kmPrice * distanceKm * scontoGiovani;
+let price = kmPrice * distanceKm;
+
+if (agePassenger > 65) {
+    price = ((kmPrice * distanceKm) - scontoTotaleA)
+} else if (agePassenger < 18) {
+    price = ((kmPrice * distanceKm) - scontoTotaleS)
+}
+
+
+
+output = `
+${price}
+`
+
+console.log(price)
+
+document.getElementById(`price`).innerHTML += price.toFixed(2);
