1f6cb24da543e02a08ed11958424442bd81b8612
Thunderbird93 <antircristian@gmail.com>
2022-10-13 15:03:27 +0200

    fix js

--- js/script.js
+++ js/script.js
@@ -13,14 +13,20 @@ let price = kmPrice * distanceKm;
 document.getElementById(`price`).innerHTML += price.toFixed(2);
 */
 
+// -------------------------------NEW------------------------------------------
 
 /*  
-<document.getElementById("btn1")> con questo creo il bottone in JS
+<document.getElementById("btn1")> con questo creo il bottone in JS usando l'ID presente in html
 <.addEventListener> con questo dico al bottone di ascoltare un evento
 <("click")> quando si clicca sul bottone, voglio che accada qualcosa
 <,function(){}> con questo gli dico "Esegui questo codice"
 */
 
 document.getElementById("btn1").addEventListener("click", function(){
-    
+    //come per il bottone, però qui prendo il "value" del bottone (ciò che ha scritto l'utente).
+    // document.getElementById("inputName").value;
+    //Qui ho preso il valore senza però conservarlo da nessuna parte.
+    // Aggiungo una variabile (let) e le do un nome (esempio: persona)
+    let persona = document.getElementById("inputName").value;
+    document.getElementById("outputName").innerHTML += persona;
 })
